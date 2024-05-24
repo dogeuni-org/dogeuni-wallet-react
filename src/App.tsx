685087fb3f21c51690979dcb0ec8dd513e6395fd
saul <ninasaul@protonamil.com>
2024-05-24 11:57:14 +0800

    feat:   addEventListener

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { useWallet } from "./provider"
 function App() {
   const { connect, address, network } = useWallet()
   return (
-    <div>
+    <div style={{ display: "flex", gap: "10px" }}>
       <button onClick={() => connect && connect()}>{address ? address : "Connect Wallet"}</button>
       <div>{network}</div>
     </div>
